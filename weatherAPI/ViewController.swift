62515ae28ad10b33c1dbb10f5df04024082695c9
MattHew Phraxayavong <mphraxa@gmail.com>
2019-08-07 07:21:27 -0700

    implemented API request complete. Displays API request

--- weatherAPI/ViewController.swift
+++ weatherAPI/ViewController.swift
@@ -13,12 +13,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var weatherLabel: UILabel!
     
     @IBAction func weatherButtonTapped(_ sender: UIButton) {
+        print("button works")
+        getWeather()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getWeather()
     }
 
+    
+    func getWeather() {
+        let session = URLSession.shared
+        let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Sacramento,us?&units=imperial&APPID=e189bc2edf10fd5264acc12fce89736e")!
+        let dataTask = session.dataTask(with: weatherURL) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                print("Error:\n\(error)")
+            } else {
+                if let data = data {
+                    let dataString = String(data: data, encoding: String.Encoding.utf8)
+                    print("All the weather data:\n\(dataString!)")
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
+                        if let mainDictionary = jsonObj!.value(forKey: "main") as? NSDictionary {
+                            if let temperature = mainDictionary.value(forKey: "temp") {
+                                DispatchQueue.main.async {
+                                    self.weatherLabel.text = "Sacramento Temperature: \(temperature)°F"
+                                }
+                            }
+                        } else {
+                            print("Error: unable to find temperature in dictionary")
+                        }
+                    } else {
+                        print("Error: unable to convert json data")
+                    }
+                } else {
+                    print("Error: did not receive data")
+                }
+            }
+        }
+        dataTask.resume()
+    }
 
 }
 
