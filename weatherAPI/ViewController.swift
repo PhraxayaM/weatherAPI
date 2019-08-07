f467ec9eb0bd322626c7ee01b13899701bee29b5
MattHew Phraxayavong <mphraxa@gmail.com>
2019-08-07 07:11:47 -0700

    initial setup complete

---
+++ weatherAPI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  weatherAPI
+//
+//  Created by MattHew Phraxayavong on 8/6/19.
+//  Copyright © 2019 Matthew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    @IBAction func weatherButtonTapped(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
